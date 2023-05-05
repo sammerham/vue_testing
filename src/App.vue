5ad8e8c1c3573c935b7b56e21132a272defb16a7
Sam Merham <sam@smerham.com>
2023-05-05 09:14:44 -0700

    created a new pokemon compoent

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import View from '@/components/View.vue'
+import MyPokemon from "./components/my-pokemon.vue";
+
 </script>
 
 <template>
   <h2>Hello world</h2>
     <View element="div">Hello from View</View>
+    <br />
+    <MyPokemon />
 </template>
 
 <style scoped>
---
+++ src/components/my-pokemon.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Pokemons</h1>
+        <button @click="getPokemon">Get Pokemon</button>
+        <div v-for="(poke, idx) in pokemons">
+            <h4 :key="idx"> {{ poke.name as  String}}</h4>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import axios from "axios";
+    import {ref} from "vue";
+    const pokemons = ref([]);
+    async function getPokemon () {
+       const res =  await axios.get("https://pokeapi.co/api/v2/pokemon");
+       const data = res.data;
+       pokemons.value = data.results;
+    };
+
+</script>
+
+<style scoped>
+
+</style>
